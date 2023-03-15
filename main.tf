7434f845e115f298ddc4609236b97d02f3dfbd0c
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2023-03-14 22:04:15 -0500

    newer container portfolios

--- main.tf
+++ main.tf
@@ -12,27 +12,37 @@ provider "azurerm" {
   client_id       = ""
   features {}
 }
-resource "azurerm_resource_group" "my-rg-test-donot-delete" {
-  location = "East US 2"
-  name     = "my-rg-test-donot-delete"
+locals {
+  base_name = "sathish"
+  rg_name   = "${local.base_name}_config_sever_rg"
 }
-resource "azurerm_storage_account" "nikerunstorage" {
+resource "azurerm_resource_group" "rg_resource_defn" {
+  location = "East US 2"
+  name     = local.rg_name
+}
+resource "azurerm_storage_account" "storage_resource_defn" {
   account_replication_type = "LRS"
   account_tier             = "Standard"
   location                 = "East US 2"
-  name                     = "nikerunstorage"
-  resource_group_name      = "my-rg-test-donot-delete"
+  name                     = "sathishrunstorageaccount"
+  resource_group_name      = local.rg_name
   account_kind             = "StorageV2"
+  depends_on               = [
+    azurerm_resource_group.rg_resource_defn
+  ]
 }
-resource "azurerm_storage_container" "nikerunsjsondata" {
+resource "azurerm_storage_container" "storage_container_defn" {
+  name                  = "sathishrunblobcontainer"
+  storage_account_name  = "sathishrunstorageaccount"
   container_access_type = "blob"
-  name                  = "nikejsondata"
-  storage_account_name  = "nikerunstorage"
+  depends_on            = [azurerm_storage_account.storage_resource_defn]
 }
-resource "azurerm_storage_blob" "nikerunsdatajson11" {
-  name                   = "nikerunsdatajson11"
-  storage_account_name   = "nikerunstorage"
-  storage_container_name = "nikerunsjsondata"
+resource "azurerm_storage_blob" "storage_blob_defn" {
+  name                   = "main.tf"
+  storage_account_name   = "sathishrunstorageaccount"
+  storage_container_name = "sathishrunblobcontainer"
   type                   = "Block"
-  source                 = "activities-11.json"
+  source                 = "main.tf"
+  depends_on             = [azurerm_storage_container.storage_container_defn]
 }
+
