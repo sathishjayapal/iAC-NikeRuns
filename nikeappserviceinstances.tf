01ccbff8d3a2b3cea32e192d745c070d9ad40f2f
Sathish Jayapal <sjayapal@skminfotech.com>
2023-04-24 22:08:22 -0500

    nikerunapp service details

---
+++ nikeappserviceinstances.tf
@@ -0,0 +1,28 @@
+resource "azurerm_service_plan" "sathish-appserviceplan" {
+  name                = "sathish-appserviceplan"
+  location            = azurerm_resource_group.rg_resource_defn.location
+  resource_group_name = azurerm_resource_group.rg_resource_defn.name
+  os_type             = var.appservice-ostype
+  sku_name            = var.appservice-sku
+
+}
+
+
+resource "azurerm_linux_web_app" "sathishnikerunswebapp" {
+  name                = "sathishconfigserverwebapp"
+  resource_group_name = azurerm_resource_group.rg_resource_defn.name
+  location            = azurerm_service_plan.sathish-appserviceplan.location
+  service_plan_id     = azurerm_service_plan.sathish-appserviceplan.id
+  logs {
+    application_logs {
+      file_system_level = "Verbose"
+    }
+  }
+  site_config {
+    application_stack {
+      docker_image     = var.dockerimagewithurl
+      docker_image_tag = var.dockerimagetag
+    }
+    always_on = false
+  }
+}
