fd33381b6a0a44ead058ada953424cf06aa30567
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2024-09-15 21:52:30 -0500

    Cleanup to modules to get postgresql

---
+++ modules/PostgreSQL/main.tf
@@ -0,0 +1,17 @@
+resource azurerm_postgresql_flexible_server "sathishdb-server" {
+    name                = join("", [var.prefix, var.main_group_name, "postgres"])
+    resource_group_name = var.rg_name
+    location            = "EASTUS"
+    sku_name            = var.sku_name
+    storage_mb          = var.storage_mb
+    administrator_password = "pas$word1234"
+    backup_retention_days = var.backup_retention_days
+    administrator_login = var.administrator_login
+    version = var.postgresql_version
+  }
+resource "azurerm_postgresql_flexible_server_firewall_rule" "sathishdbfw" {
+    name             = "sathishdb-fw"
+    server_id        = azurerm_postgresql_flexible_server.sathishdb-server.id
+    start_ip_address = "134.215.150.123"
+    end_ip_address   = "134.215.150.123"
+}
