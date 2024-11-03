1aef913bf969145c97a1156a390f2c01744da276
Sathish K Jayapal <sathishk.dot@gmail.com>
2024-11-02 21:28:16 -0500

    gitignore

--- modules/PostgreSQL/main.tf
+++ modules/PostgreSQL/main.tf
@@ -4,7 +4,7 @@ resource azurerm_postgresql_flexible_server "sathishdb-server" {
     location            = "EASTUS"
     sku_name            = var.sku_name
     storage_mb          = var.storage_mb
-    administrator_password = "pas$word1234"
+    administrator_password = ""
     backup_retention_days = var.backup_retention_days
     administrator_login = var.administrator_login
     version = var.postgresql_version
