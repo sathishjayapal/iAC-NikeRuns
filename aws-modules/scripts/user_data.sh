4a29790b55db7d455431a36b43ce618dc6ad7857
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2024-11-02 21:20:55 -0500

    EC2 provision

---
+++ aws-modules/scripts/user_data.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo yum update -y
+sudo yum install -y httpd.x86_64
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+sudo yum install java-21-amazon-corretto-headless -y
