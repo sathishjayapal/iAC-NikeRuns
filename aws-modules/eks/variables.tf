095314b4d7fc65c6d31f9763bbc49b28f922fad8
Sathish Jayapal <203055+sathishjayapal@users.noreply.github.com>
2025-11-30 19:20:30 -0600

    added k8s configuration

--- aws-modules/eks/variables.tf
+++ aws-modules/eks/variables.tf
@@ -15,7 +15,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.32"
+  default     = "1.34"
 }
 
 ########################################
